d299aa1fc191b98ff4b5187334bceb426b4d43ce
shawsk0723 <shawsk0723@gmail.com>
2023-03-15 18:29:46 +0900

    Update AnalysisResultSaver.py

--- app/AnalysisResultSaver.py
+++ app/AnalysisResultSaver.py
@@ -11,3 +11,7 @@ import pandas as pd
 def saveAnalsisResultToCsv(result, outFilePath):
     df = pd.DataFrame(result)
     df.to_csv(outFilePath, index=False)
+
+    df_cleaned = df.loc[df['buy score'] != "N/A" ]
+    df_sorted = df_cleaned.sort_values('buy score', ascending=False)
+    df_sorted.to_csv(outFilePath.replace('.csv', '_sorted_buyscore.csv'), index=False)
