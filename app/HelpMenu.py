76bc726db7b6cdfffdb23e59ba39816cf7feab3d
shawsk0723 <shawsk0723@gmail.com>
2023-03-15 18:30:17 +0900

    Update HelpMenu.py

--- app/HelpMenu.py
+++ app/HelpMenu.py
@@ -8,10 +8,10 @@ Author
 
 from tkinter import messagebox
 import Config
-import ExpiryChecker
+from ExpiryChecker import *
 
 def showAbout():
     aboutMsg = f"{Config.WIN_TITLE}\n\n"
     aboutMsg += "제작자: 코드장인\n\n"
-    aboutMsg += f"유효 기간: {ExpiryChecker.getExiryDate()}"
+    aboutMsg += f"유효 기간: {HardExpiryChecker().getExiryDate()}"
     messagebox.showinfo(message=aboutMsg)
