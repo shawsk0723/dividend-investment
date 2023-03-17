e03c874964f249109942c1dc6279d1c1c653edfb
shawsk0723 <shawsk0723@gmail.com>
2023-03-17 14:58:12 +0900

    Update HelpMenu.py

--- app/HelpMenu.py
+++ app/HelpMenu.py
@@ -7,11 +7,17 @@ Author
 """
 
 from tkinter import messagebox
-import Config
 from ExpiryChecker import *
+import Config
+
+
+CAFE_URL = 'https://cafe.naver.com/codegarage'
+BLOG_URL = 'https://blog.naver.com/shawgibal'
 
 def showAbout():
     aboutMsg = f"{Config.WIN_TITLE}\n\n"
     aboutMsg += "제작자: 코드장인\n\n"
-    aboutMsg += f"유효 기간: {HardExpiryChecker().getExiryDate()}"
-    messagebox.showinfo(message=aboutMsg)
+    aboutMsg += f"카페: {CAFE_URL}\n\n"
+    aboutMsg += f"블로그: {BLOG_URL}\n\n"
+    #aboutMsg += f"유효 기간: {HardExpiryChecker().getExiryDate()}"
+    messagebox.showinfo(title=Config.APP_NAME, message=aboutMsg)
